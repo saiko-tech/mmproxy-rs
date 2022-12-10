890eae1b808d0c79a4ab36e6bc2fce047edf5ea5
brkp <brkp@tutanota.com>
2022-12-10 19:50:17 +0300

    setup tokio

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,10 @@
 use env_logger::{Env, DEFAULT_FILTER_ENV};
+use std::io;
 
-fn main() {
+#[tokio::main]
+async fn main() -> io::Result<()> {
     env_logger::init_from_env(Env::default().filter_or(DEFAULT_FILTER_ENV, "debug"));
     log::info!("Hello, World!");
+
+    Ok(())
 }
