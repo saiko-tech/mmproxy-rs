3aa27e3288274c57fbbec368c8fe790942ffc7f3
brkp <brkp@tutanota.com>
2022-12-13 23:53:24 +0300

    better log messages

--- src/tcp.rs
+++ src/tcp.rs
@@ -20,13 +20,11 @@ pub async fn listen(args: Args) -> io::Result<()> {
 
     loop {
         let (conn, addr) = listener.accept().await?;
-        log::info!("new connection: {addr}");
-
         if let Some(ref allowed_subnets) = args.allowed_subnets {
             let ip_addr = addr.ip();
 
             if !check_origin_allowed(&ip_addr, allowed_subnets) {
-                log::debug!("connection origin is not allowed");
+                log::debug!("connection origin is not allowed: {ip_addr}");
                 continue;
             }
         }
@@ -61,12 +59,9 @@ async fn tcp_handle_connection(
         SocketAddr::V4(_) => args.ipv4_fwd,
         SocketAddr::V6(_) => args.ipv6_fwd,
     };
-    log::info!("source addr: {src_addr}");
-    log::info!("target addr: {target_addr}");
+    log::info!("[new conn] [origin: {addr} [src: {src_addr}]");
 
     let mut upstream_conn = tcp_create_upstream_conn(src_addr, target_addr, args.mark).await?;
-    log::info!("created the upstream connection");
-
     conn.set_nodelay(true)?;
 
     tokio::io::copy_buf(&mut rest, &mut upstream_conn).await?;
